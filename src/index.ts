be0862d7ad701d7c10bc6fd11c152f208811e712
AmbarAdriazola <ambar.a.q35@gmail.com>
2022-05-01 01:21:46 -0400

    ojala funcione

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import 'dotenv/config'
 import app  from './app'
 
-const PORT = process.env.PORT || 4000
+const  PORT = process.env.PORT || 4000
 
 app.listen(PORT, () => {
   console.log (`Server is listening on port ${PORT}`)
